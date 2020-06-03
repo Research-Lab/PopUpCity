687a2d2d966e7f08dcd3c3792df5db2f21a1e793
hannahyg <65688129+hannahyg@users.noreply.github.com>
2020-06-03 17:26:10 -0400

    Update ThinFilm.m

--- ThinFilm.m
+++ ThinFilm.m
@@ -1,4 +1,4 @@
-% 1 month simulation for January using a thin film solar panel
+% 1 year simulation using a thin film solar panel
 % http://www.firstsolar.com/-/media/First-Solar/Technical-Documents/Series-6-Datasheets/Series-6-Datasheet.ashx
 % Last Modified May 24, 2020
 
@@ -6,7 +6,7 @@ panelsize = 2.47; %m^2
 npanel = 18.2/100; % FS-6450 FS-6450A
 pmax = 450; %W 5
 
-data = xlsread('Brandon');
+data = xlsread(fullfile(path, file));
 SolarIn = data(:,6);
 
 PowerProducedThinFilm = pmax*npanel*SolarIn*panelsize; %kW
